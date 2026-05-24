4f12c21e0e4237010002d367af61c06a9335865b
Noah Gullo <noahgulloingram@gmail.com>
2026-05-24 15:46:47 -0700

    feat: add point class, location to ships, and more test

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -1,18 +1,22 @@
-import { Ship } from "../src/index.js";
+import { Point, Ship } from "../src/index.js";
 
 test("Ship creation", () => {
-    expect(new Ship(5)).toMatchObject({length: 5, hitCount: 0});
-    expect(new Ship(3)).toMatchObject({length: 3, hitCount: 0});
+    expect(new Ship(5, new Point(0,0))).toMatchObject({length: 5, hitCount: 0, location: new Point(0,0)});
+    expect(new Ship(3, new Point(2,3))).toMatchObject({length: 3, hitCount: 0, location: new Point(2,3)});
 })
 
 test("Invalid ship creation", () => {
-    expect(() => {Ship(5)}).toThrow();
-    expect(() => {new Ship(-1)}).toThrow();
-    expect(() => {new Ship(0)}).toThrow();
+    expect(() => {Ship(5, new Point(0,0))}).toThrow();
+    expect(() => {new Ship(-1, new Point(0,0))}).toThrow();
+    expect(() => {new Ship(0, new Point(0,0))}).toThrow();
+    expect(() => {new Ship(5, new Point(-1,4))}).toThrow();
+    expect(() => {new Ship(5, new Point(10,4))}).toThrow();
+    expect(() => {new Ship(5, new Point(2,-1))}).toThrow();
+    expect(() => {new Ship(5, new Point(2,10))}).toThrow();
 })
 
 test("Ship hit increments hitCount", () => {
-    const ship = new Ship(4);
+    const ship = new Ship(4, new Point(0,0));
     expect(ship.hitCount).toBe(0);
     ship.hit();
     expect(ship.hitCount).toBe(1);
@@ -22,11 +26,31 @@ test("Ship hit increments hitCount", () => {
 })
 
 test("Ship correctly updates sunk status", () => {
-    const ship = new Ship(3);
+    const ship = new Ship(3, new Point(0,0));
     expect(ship.isSunk()).toBe(false);
     ship.hit();
     ship.hit();
     expect(ship.isSunk()).toBe(false);
     ship.hit();
     expect(ship.isSunk()).toBe(true);
+})
+
+test("Valid points on the grid", () => {
+    expect(new Point(0,0).onGrid()).toBe(true);
+    expect(new Point(9,0).onGrid()).toBe(true);
+    expect(new Point(0,9).onGrid()).toBe(true);
+    expect(new Point(9,9).onGrid()).toBe(true);
+
+    expect(new Point(5,3).onGrid()).toBe(true);
+    expect(new Point(2,8).onGrid()).toBe(true);
+    expect(new Point(7,4).onGrid()).toBe(true);
+})
+
+test("Invalid points on the grid", () => {
+    expect(new Point(-1,5).onGrid()).toBe(false);
+    expect(new Point(10,5).onGrid()).toBe(false);
+    expect(new Point(5,-1).onGrid()).toBe(false);
+    expect(new Point(5,-10).onGrid()).toBe(false);
+    expect(new Point(-1,-5).onGrid()).toBe(false);
+    expect(new Point(10,10).onGrid()).toBe(false);
 })
--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import "./styles.css";
 
+export class Point{
+    #x;
+    #y;
+    constructor(x, y){
+        this.#x = x;
+        this.#y = y;
+    }
+
+    onGrid(){
+        if(this.#x < 0 || this.#x > 9 || this.#y < 0 || this.#y > 9){
+            return false;
+        }
+        return true;
+    }
+}
+
 export class Ship{
     #length;
+    #location;
     #hitCount;
     #sunk;
-    constructor(length){
+    constructor(length, location){
         if(!new.target) throw new Error("Cannot call Ship constructor without 'new'");
         if(length < 1) throw new Error("Length must be greater than 0");
+        if(!(location instanceof Point)) throw new Error("Location must be a Point instance.");
+        if(!location.onGrid()) throw new Error("Ship must be located on 10x10 grid starting at (0,0)."); 
         this.#length = length;
+        this.#location = location;
         this.#hitCount = 0;
         this.#sunk = false; 
     }
@@ -28,6 +48,10 @@ export class Ship{
         return this.#length;
     }
 
+    get location(){
+        return this.#location;
+    }
+    
     get hitCount(){
         return this.#hitCount;
     }
